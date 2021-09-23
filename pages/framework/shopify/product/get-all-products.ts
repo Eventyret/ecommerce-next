11461a12364d929b5c9cbba195ff22b66e9fad9e
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-23 20:33:31 +0100

    Mapped over edges to return prodcuts

--- pages/framework/shopify/product/get-all-products.ts
+++ pages/framework/shopify/product/get-all-products.ts
@@ -7,6 +7,7 @@ type ReturnType = {
 };
 const getAllProducts = async (): Promise<any> => {
   const { data } = await fetchAPI<ReturnType>({ query: getAllProductsQuery });
-  return data.products;
+  const products = data.products.edges.map(({ node: product }) => product) ?? [];
+  return products;
 };
 export default getAllProducts;
