8482b6e866d1b959126f4a845d28a5819128f480
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-23 16:01:09 +0100

    Added inferStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
+import type { InferGetStaticPropsType } from 'next';
+
+export async function getStaticProps() {
+  const products = [1, 2, 3];
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
   const message: string = 'Hello World';
-  return <div>{message}</div>;
+  return <div>{products}</div>;
 }
