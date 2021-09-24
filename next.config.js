2d59f0aa22dfac52c1355f0005dc9aee2be25998
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-24 10:15:57 +0100

    Made framework more unique and ability to use specific frameworks

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const { withFrameworkConfig } = require('./framework/common/config');
+
+module.exports = withFrameworkConfig({
+  i18n: {
+    locales: ['en-GB', 'en-US', 'es'],
+    defaultLocale: 'en-GB',
+  },
   reactStrictMode: true,
-}
+});
+console.log('next.config.js', JSON.stringify(module.exports, null, 2));
