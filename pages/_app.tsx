df187c1c275102e9e838730798aa0dcdffde66f8
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-24 11:40:55 +0100

    Made div a fragment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import { AppProps } from 'next/app';
 
 function MainApp({ Component, pageProps }: AppProps) {
   return (
-    <div className='app-component'>
+    <>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
 
