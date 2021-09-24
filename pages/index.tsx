4f9a5120805ae9a39228a3022ad2fce424b9832c
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-24 11:51:22 +0100

    Linted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
+import { getConfig } from '@framework/api/config';
 import getAllProducts from 'framework/shopify/product/get-all-products';
 import type { InferGetStaticPropsType } from 'next';
-import { getConfig } from '@framework/api/config';
 export async function getStaticProps() {
   const config = getConfig();
   const products = await getAllProducts(config);
