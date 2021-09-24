0f16c8d739f6c1a90886e41d654f0fd19a054acf
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-24 09:23:24 +0100

    Normalized products from shopify

--- pages/framework/shopify/product/get-all-products.ts
+++ pages/framework/shopify/product/get-all-products.ts
@@ -1,13 +1,13 @@
 import fetchAPI from '../utils/fetch-api';
 import getAllProductsQuery from '../utils/queries/get-all-products';
 import { ProductConnection } from '../schema';
-
+import { normalizeProduct } from '../utils/normalize';
 type ReturnType = {
   products: ProductConnection;
 };
 const getAllProducts = async (): Promise<any> => {
   const { data } = await fetchAPI<ReturnType>({ query: getAllProductsQuery });
-  const products = data.products.edges.map(({ node: product }) => product) ?? [];
+  const products = data.products.edges.map(({ node: product }) => normalizeProduct(product)) ?? [];
   return products;
 };
 export default getAllProducts;
