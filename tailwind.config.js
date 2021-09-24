34f73dbf8ebcddddeb0d4c68a744dfbaad27c03e
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-24 12:07:33 +0100

    Added autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   jit: true,
   darkMode: false, // or 'media' or 'class'
   theme: {
