9189c2d01705f4e8a08cad4ca211d29cc1869961
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-24 10:26:21 +0100

    tsconfig now changes programatically

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('deepmerge');
+const fs = require('fs');
 
 function withFrameworkConfig(defaultConfig = {}) {
   const framework = 'shopify';
@@ -7,6 +8,12 @@ function withFrameworkConfig(defaultConfig = {}) {
   const frameworkNextConfig = require(path.join('../', framework, 'next.config'));
   const config = merge(defaultConfig, frameworkNextConfig);
 
+  const tsPath = path.join(process.cwd(), 'tsconfig.json');
+  const tsConfig = require(tsPath);
+  tsConfig.compilerOptions.paths['@framework'] = [`framework/${framework}`];
+  tsConfig.compilerOptions.paths['@framework/*'] = [`framework/${framework}/*`];
+
+  fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2));
   return config;
 }
 
