e77fa5aa1849a6cb2593c1788cbaba63e42ebd5f
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 21:38:51 +0100

    Normalized values for hex and name

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -57,5 +57,18 @@ const normalizeProductOption = ({
   name: displayName,
   values,
 }: ProductOption) => {
-  return {};
+  const normalized = {
+    id,
+    displayName,
+    values: values.map((value) => {
+      let output: any = {
+        label: value,
+      };
+      if (displayName.match(/colou?r/gi)) {
+        output = { ...output, hexColor: value };
+      }
+      return output;
+    }),
+  };
+  return normalized;
 };
