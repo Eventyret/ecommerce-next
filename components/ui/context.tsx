a4a879508a0c00d685ba1b0a14606aaf505bcbb1
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 17:14:49 +0100

    Added useState hook

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,13 +1,18 @@
-import { createContext, FC, ReactNode, useContext } from 'react';
+import { createContext, FC, ReactNode, useContext, useState } from 'react';
 
-const UIContext = createContext<{ [key: string]: string }>({
+const UIContext = createContext<{ [key: string]: any }>({
   uiState: 'defaultState',
 });
 
 export const UIProvider: FC<ReactNode> = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const uiState = {
+    isSidebarOpen,
+    setIsSidebarOpen,
+  };
   return (
     <>
-      <UIContext.Provider value={{ uiState: 'Something for now' }}>
+      <UIContext.Provider value={{ uiState: uiState }}>
         {children}
       </UIContext.Provider>
     </>
