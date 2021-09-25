d594352f940c761cf5dc71ec191ce8be68c38fce
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 15:33:45 +0100

    Started styling

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,12 +1,28 @@
 import { FC } from 'react';
 import { Container } from '@components/ui';
-
+import Link from 'next/link';
+import s from './Navbar.module.css';
 const Navbar: FC = () => {
   return (
     <Container>
-      <div className="flex flex-row md:py-6">
-        <h1>NEXT_STORE</h1>
-      </div>
+      <div className={s.root}></div>
+      <Link href="/">
+        <a className={s.logo}>NEXT_STORE</a>
+      </Link>
+      <nav>
+        <Link href="/">
+          <a className={s.link}>All</a>
+        </Link>
+        <Link href="/">
+          <a className={s.link}>Clothes</a>
+        </Link>
+        <Link href="/">
+          <a className={s.link}>Accessories</a>
+        </Link>
+        <Link href="/">
+          <a className={s.link}>Shoes</a>
+        </Link>
+      </nav>
     </Container>
   );
 };
