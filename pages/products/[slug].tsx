4cab9c6721bc8bc59744ae94c2a496c64b0c253c
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 20:18:58 +0100

    Changed to dynamically get slugs

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,4 +1,6 @@
 import { Layout } from '@components/common';
+import { getConfig } from '@framework/api/config';
+import getAllProductsPaths from '@framework/product/get-all-products-paths';
 import {
   GetStaticPaths,
   GetStaticProps,
@@ -8,12 +10,10 @@ import {
 
 // Fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
+  const config = getConfig();
+  const { products } = await getAllProductsPaths(config);
   return {
-    paths: [
-      { params: { slug: 'cool-hat' } },
-      { params: { slug: 'lightweight-jacket' } },
-      { params: { slug: 't-shirt' } },
-    ],
+    paths: products.map((product) => ({ params: { slug: product.slug } })),
     fallback: false,
   };
 };
