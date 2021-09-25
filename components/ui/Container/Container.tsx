d4fdea1fb304612617d7b41e0cc192164fa351f8
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 14:29:07 +0100

    Added custom style for testing

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -4,6 +4,10 @@ interface Props {
   children: ReactNode | ReactNode[];
 }
 const Container: FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div style={{ maxWidth: '1920px' }} className="px-6 mx-auto">
+      {children}
+    </div>
+  );
 };
 export default Container;
