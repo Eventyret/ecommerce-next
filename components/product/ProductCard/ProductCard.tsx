bb3f0f7351f2f459ec854fa33978127139eef7f3
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 10:19:00 +0100

    Added placeholder

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,11 +1,35 @@
 import { Product } from '@common/types/product';
 import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
   product: Product;
 }
+const placeHolderImage = '/product-image-placeholder.svg';
 const ProductCard: FC<Props> = ({ product }) => {
-  return <div>{product.name}</div>;
+  return (
+    <Link href={`/product/${product.slug}`}>
+      <a>
+        <div>
+          <h3>
+            <span>{product.name}</span>
+          </h3>
+          <span>£ 14</span>
+        </div>
+        {product.images && (
+          <Image
+            src={placeHolderImage}
+            alt={product.name ?? 'Product Image'}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )}
+      </a>
+    </Link>
+  );
 };
 
 export default ProductCard;
