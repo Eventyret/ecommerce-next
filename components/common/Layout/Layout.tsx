72538ed6c64985e23fcee419f38cc946e596ea7a
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 15:30:42 +0100

    Added basic navbar without styling

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 import s from './Layout.module.css';
-import { Footer } from '@components/common';
+import { Footer, Navbar } from '@components/common';
 const Layout: FC = ({ children }) => {
   return (
     <div className={s.root}>
+      <Navbar />
       <main className="fit">{children}</main>
       <Footer />
     </div>
