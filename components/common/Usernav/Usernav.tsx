17ac3242f88c9bae33cdf7f4973600f0bce601d3
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 16:01:49 +0100

    Finished usernav

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react';
 import s from './Usernav.module.css';
 import Link from 'next/link';
+import { Bag as Cart, Heart } from '@components/icons';
 const Usernav: FC<{}> = () => {
   return (
     <nav>
       <ul className={s.list}>
-        <li className={s.item}>Cart</li>
+        <li className={s.item}>
+          <Cart />
+        </li>
         <li className={s.item}>
           <Link href="/">
-            <a> Whishlist</a>
+            <a>
+              <Heart />
+            </a>
           </Link>
         </li>
       </ul>
