eb1d1a7dac50a5189e93a245bd51452b60306f2b
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 22:09:13 +0100

    Moved from testing to correct styles

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,4 +1,5 @@
 import { Layout } from '@components/common';
+import { ProductView } from '@components/product';
 import { Container } from '@components/ui';
 import { getConfig } from '@framework/api/config';
 import { getAllProductsPaths, getProduct } from '@framework/product';
@@ -38,49 +39,7 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return (
-    <Container>
-      <p>id: {product?.id}</p>
-      <p>name: {product?.name}</p>
-      <p>price value: {product?.price.value}</p>
-      <p>price currency: {product?.price.currencyCode}</p>
-      <p>description: {product?.description}</p>
-
-      <h1 className="mb-4">OPTIONS</h1>
-      <div>
-        {product?.options.map((option) => (
-          <div key={option.id}>
-            <p>Name: {option.displayName}</p>
-            {option.values.map((value) => (
-              <div key={value.label}>
-                <p>Label: {value.label}</p>
-                <p>Hex Color: {value.hexColor}</p>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      <h1 className="mb-4">VARIANTS</h1>
-      <div>
-        {product?.variants.map((variant) => (
-          <div key={variant.id}>
-            <p>Variant Name: {variant.name}</p>
-            {variant.options.map((vo) => (
-              <div key={vo.id}>
-                <p>Name: {vo.displayName}</p>
-                {vo.values.map((value) => (
-                  <div key={value.label}>
-                    <p>Label: {value.label}</p>
-                    <p>Hexcolor: {value.hexColor}</p>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </Container>
-  );
+  return <>{product && <ProductView product={product} />}</>;
 }
 
 ProductSlug.layout = Layout;
