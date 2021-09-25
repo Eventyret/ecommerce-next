dc2a8c845ee5c31878685d73f3cb1a8ed69bacec
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 14:48:06 +0100

    Added module and added style

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,10 +1,10 @@
 import { FC, ReactNode } from 'react';
-
+import s from './Marquee.module.css';
 interface Props {
   children: ReactNode[];
 }
 
 const Marquee: FC<Props> = ({ children }) => {
-  return <div className="">{children}</div>;
+  return <div className={s.root}>{children}</div>;
 };
 export default Marquee;
