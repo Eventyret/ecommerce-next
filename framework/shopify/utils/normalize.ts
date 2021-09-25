2fde3ffd761f01d7f3b80ae8e2bf0a2bdb04b404
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 22:00:52 +0100

    Fixed an issue with normalize not working

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,4 +1,3 @@
-import { Product } from 'framework/common/types/product';
 import {
   ImageEdge,
   MoneyV2,
@@ -7,6 +6,70 @@ import {
   ProductVariantConnection,
   SelectedOption,
 } from '../schema';
+
+import { Product } from '@common/types/product';
+
+const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
+  edges.map(({ node: { originalSrc: url, ...rest } }) => ({
+    url: `/images/${url}`,
+    ...rest,
+  }));
+
+const normalizeProductPrice = ({ currencyCode, amount }: MoneyV2) => ({
+  value: +amount,
+  currencyCode,
+});
+
+const normalizeProductOption = ({
+  id,
+  values,
+  name: displayName,
+}: ProductOption) => {
+  const normalized = {
+    id,
+    displayName,
+    values: values.map((value) => {
+      let output: any = {
+        label: value,
+      };
+
+      if (displayName.match(/colou?r/gi)) {
+        output = {
+          ...output,
+          hexColor: value,
+        };
+      }
+
+      return output;
+    }),
+  };
+
+  return normalized;
+};
+
+const normalizeProductVariants = ({ edges }: ProductVariantConnection) => {
+  return edges.map(({ node }) => {
+    const { id, selectedOptions, sku, title, priceV2, compareAtPriceV2 } = node;
+
+    return {
+      id,
+      name: title,
+      sku: sku || id,
+      price: +priceV2.amount,
+      listPrice: +compareAtPriceV2?.amount,
+      requiresShipping: true,
+      options: selectedOptions.map(({ name, value }: SelectedOption) => {
+        const option = normalizeProductOption({
+          id,
+          name,
+          values: [value],
+        });
+
+        return option;
+      }),
+    };
+  });
+};
 
 export function normalizeProduct(productNode: ShopifyProduct): Product {
   const {
@@ -33,8 +96,8 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
     price: normalizeProductPrice(priceRange.minVariantPrice),
     options: options
       ? options
-          .filter((option) => option.name !== 'Title')
-          .map((option) => normalizeProductOption(option))
+          .filter((o) => o.name !== 'Title')
+          .map((o) => normalizeProductOption(o))
       : [],
     variants: variants ? normalizeProductVariants(variants) : [],
     ...rest,
@@ -42,55 +105,3 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
 
   return product;
 }
-
-const normalizeProductPrice = ({ currencyCode, amount }: MoneyV2) => {
-  return {
-    value: +amount,
-    currencyCode,
-  };
-};
-
-const normalizeProductImages = ({ edges }: { edges: ImageEdge[] }) =>
-  edges.map(({ node: { originalSrc: url, ...rest } }) => ({
-    url: `/images/${url}`,
-    ...rest,
-  }));
-
-const normalizeProductOption = ({
-  id,
-  name: displayName,
-  values,
-}: ProductOption) => {
-  const normalized = {
-    id,
-    displayName,
-    values: values.map((value) => {
-      let output: any = {
-        label: value,
-      };
-      if (displayName.match(/colou?r/gi)) {
-        output = { ...output, hexColor: value };
-      }
-      return output;
-    }),
-  };
-  return normalized;
-};
-
-const normalizeProductVariants = ({ edges }: ProductVariantConnection) => {
-  edges.map(({ node }) => {
-    const { id, selectedOptions, sku, title, priceV2, compareAtPriceV2 } = node;
-    return {
-      id,
-      name: title,
-      sku: sku || id,
-      price: +priceV2.amount,
-      listPrice: +compareAtPriceV2?.amount,
-      requiresShipping: true,
-      options: selectedOptions.map(({ name, value }: SelectedOption) => {
-        const option = normalizeProductOption({ id, name, values: [value] });
-        return option;
-      }),
-    };
-  });
-};
