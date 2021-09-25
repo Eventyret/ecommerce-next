08d3f9b3a5b2e19ac79eb2e139eb50ab0c7690b5
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 14:25:31 +0100

    Created container component

---
+++ components/ui/Container/Container.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode[];
+}
+const Container: FC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
+export default Container;
