f55d6187317584e2a5aadf72b7efcb6d6ef400f5
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 09:49:00 +0100

    Added product card and passing props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { getConfig } from '@framework/api/config';
 import getAllProducts from 'framework/shopify/product/get-all-products';
 import type { InferGetStaticPropsType } from 'next';
 import { Layout } from '@components/common';
+import { ProductCard } from '@components/product';
 export async function getStaticProps() {
   const config = getConfig();
   const products = await getAllProducts(config);
@@ -14,7 +15,13 @@ export async function getStaticProps() {
 }
 
 export default function Home({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div className='root'>{JSON.stringify(products)}</div>;
+  return (
+    <div className='root'>
+      {products.slice(0, 3).map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
+  );
 }
 
 Home.Layout = Layout;
