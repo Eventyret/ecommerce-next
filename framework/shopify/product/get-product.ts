42b39f74816c81ca88fadeec184731ea08045566
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 21:06:59 +0100

    Now fetching the correct data depending on slug

--- framework/shopify/product/get-product.ts
+++ framework/shopify/product/get-product.ts
@@ -1,10 +1,15 @@
 import { ApiConfig } from '@common/types/api';
 import { getProductQuery } from '@framework/utils';
 
-const getProduct = async (config: ApiConfig): Promise<any> => {
+const getProduct = async (options: {
+  config: ApiConfig;
+  variables: any;
+}): Promise<any> => {
+  const { config, variables } = options;
   const { data } = await config.fetch<any>({
     query: getProductQuery,
     url: config.apiURL,
+    variables,
   });
   return {
     product: {
