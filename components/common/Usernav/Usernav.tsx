91197fece5af9969b322aa709edcafcd683f1cfd
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 15:50:16 +0100

    Added usernav component

---
+++ components/common/Usernav/Usernav.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import s from './Usernav.module.css';
+import Link from 'next/link';
+const UserNav: FC<{}> = () => {
+  return (
+    <nav>
+      <ul className={s.list}>
+        <li className={s.item}>Cart</li>
+        <li className={s.item}>
+          <Link href="/">
+            <a> Whishlist</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default UserNav;
