18494972a1a865a174935a56af33531e814a9765
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 14:36:33 +0100

    Added Type to custom element

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -1,8 +1,8 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, ComponentType, HTMLAttributes } from 'react';
 
 interface Props {
   children: ReactNode | ReactNode[];
-  el?: any;
+  el?: ComponentType<HTMLAttributes<HTMLElement>>;
 }
 const Container: FC<Props> = ({ children, el: Component = 'div' }) => {
   return <Component className="root">{children}</Component>;
