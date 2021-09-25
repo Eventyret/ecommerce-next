1b4f5c6a49fb9750d5996f6e5729085746895fdb
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 22:01:00 +0100

    Fixed slug to render

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,4 +1,5 @@
 import { Layout } from '@components/common';
+import { Container } from '@components/ui';
 import { getConfig } from '@framework/api/config';
 import { getAllProductsPaths, getProduct } from '@framework/product';
 import {
@@ -6,6 +7,7 @@ import {
   GetStaticPropsContext,
   InferGetStaticPropsType,
 } from 'next';
+import React from 'react';
 
 // Fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -36,7 +38,49 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>{JSON.stringify(product, null, 2)}</div>;
+  return (
+    <Container>
+      <p>id: {product?.id}</p>
+      <p>name: {product?.name}</p>
+      <p>price value: {product?.price.value}</p>
+      <p>price currency: {product?.price.currencyCode}</p>
+      <p>description: {product?.description}</p>
+
+      <h1 className="mb-4">OPTIONS</h1>
+      <div>
+        {product?.options.map((option) => (
+          <div key={option.id}>
+            <p>Name: {option.displayName}</p>
+            {option.values.map((value) => (
+              <div key={value.label}>
+                <p>Label: {value.label}</p>
+                <p>Hex Color: {value.hexColor}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <h1 className="mb-4">VARIANTS</h1>
+      <div>
+        {product?.variants.map((variant) => (
+          <div key={variant.id}>
+            <p>Variant Name: {variant.name}</p>
+            {variant.options.map((vo) => (
+              <div key={vo.id}>
+                <p>Name: {vo.displayName}</p>
+                {vo.values.map((value) => (
+                  <div key={value.label}>
+                    <p>Label: {value.label}</p>
+                    <p>Hexcolor: {value.hexColor}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 }
 
 ProductSlug.layout = Layout;
