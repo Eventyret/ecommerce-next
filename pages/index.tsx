ec259c6f734debda352b942e158aa2fdf2282991
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 14:19:16 +0100

    Addd hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import { Layout } from '@components/common';
+import { ProductCard } from '@components/product';
+import { Grid, Hero } from '@components/ui';
 import { getConfig } from '@framework/api/config';
 import getAllProducts from 'framework/shopify/product/get-all-products';
 import type { InferGetStaticPropsType } from 'next';
-import { Layout } from '@components/common';
-import { ProductCard } from '@components/product';
-import { Grid } from '@components/ui';
+
 export async function getStaticProps() {
   const config = getConfig();
   const products = await getAllProducts(config);
@@ -25,6 +26,10 @@ export default function Home({
           <ProductCard product={product} key={product.id} />
         ))}
       </Grid>
+      <Hero
+        headline="Cookies, ice cream and muffin"
+        description="Marshmallow tart jelly icing cotton candy tootsie roll cotton candy candy canes. Cake liquorice sesame snaps. Cupcake cake cheesecake pie marshmallow lollipop soufflé marshmallow dessert. Cheesecake jujubes halvah chupa chups lollipop tootsie roll. Jelly-o tiramisu jelly toffee cake croissant lemon drops pudding. Donut sesame snaps gummi bears toffee. Sesame snaps jelly-o oat cake chocolate marzipan cake lollipop. Gingerbread cheesecake jujubes fruitcake cake. Tiramisu cotton candy marzipan candy canes oat cake pudding bonbon."
+      />
     </>
   );
 }
