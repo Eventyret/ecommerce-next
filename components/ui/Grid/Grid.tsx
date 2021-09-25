543b7ab8bfa25bf71391eccb7d00ed8e4d748b15
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 12:13:06 +0100

    Added postcss nesting

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,7 +1,8 @@
 import { FC, ReactNode } from 'react';
+import s from './Grid.module.css';
 
 const Grid: FC<ReactNode> = ({ children }) => {
-  return <div className="root-grid">{children}</div>;
+  return <div className={s.root}>{children}</div>;
 };
 
 export default Grid;
