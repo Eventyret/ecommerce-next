5dec5450b8aa74a2b996b4cd818f0455e84bead2
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 13:59:06 +0100

    Styled products

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from '@common/types/product';
 import { FC } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import s from './ProductCard.module.css';
 
 interface Props {
   product: Product;
@@ -10,15 +11,17 @@ const placeHolderImage = '/product-image-placeholder.svg';
 const ProductCard: FC<Props> = ({ product }) => {
   return (
     <Link href={`/product/${product.slug}`}>
-      <a>
-        <div>
-          <h3>
+      <a className={s.root}>
+        <div className={s.productBg}></div>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>£ 14</span>
+          <span className={s.productPrice}>£ 14</span>
         </div>
         {product.images && (
           <Image
+            className={s.productImage}
             src={product.images[0].url ?? placeHolderImage}
             alt={product.name ?? 'Product Image'}
             height={540}
