0c1b37047c29a638ee115347899dc907273b1e84
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-25 15:37:35 +0100

    Styled icons

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -5,24 +5,27 @@ import s from './Navbar.module.css';
 const Navbar: FC = () => {
   return (
     <Container>
-      <div className={s.root}></div>
-      <Link href="/">
-        <a className={s.logo}>NEXT_STORE</a>
-      </Link>
-      <nav>
-        <Link href="/">
-          <a className={s.link}>All</a>
-        </Link>
-        <Link href="/">
-          <a className={s.link}>Clothes</a>
-        </Link>
-        <Link href="/">
-          <a className={s.link}>Accessories</a>
-        </Link>
-        <Link href="/">
-          <a className={s.link}>Shoes</a>
-        </Link>
-      </nav>
+      <div className={s.root}>
+        <div className="flex items-center flex-1">
+          <Link href="/">
+            <a className={s.logo}>NEXT_STORE</a>
+          </Link>
+          <nav className="ml-6 space-x-6">
+            <Link href="/">
+              <a className={s.link}>All</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Clothes</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Accessories</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Shoes</a>
+            </Link>
+          </nav>
+        </div>
+      </div>
     </Container>
   );
 };
