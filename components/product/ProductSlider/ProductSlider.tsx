b33e1ca6e2343de62bcf336d1ca9c7b00f5ceaa5
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-26 15:49:18 +0100

    Started impllement useKeenSlider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,14 +1,28 @@
-import React, { FC, Children, isValidElement } from 'react';
+import React, { FC, Children, isValidElement, useState } from 'react';
 import s from './ProductSlider.module.css';
+import { useKeenSlider } from 'keen-slider/react';
 
 const ProductSlider: FC = ({ children }) => {
+  const [sliderRef, _] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(s) {
+      s.details().relativeSlide;
+    },
+  });
+
   return (
     <div className={s.root}>
-      <div className="h-full transition-opacity keen-slider">
+      <div
+        ref={sliderRef as any}
+        className="h-full transition-opacity keen-slider"
+      >
         {Children.map(children, (child) => {
           if (isValidElement(child)) {
             return React.cloneElement(child, {
-              className: 'keen-slider__slide',
+              className: `${
+                child.props.className ? `${child.props.className}` : ''
+              } keen-slider__slide`,
             });
           }
           return child;
