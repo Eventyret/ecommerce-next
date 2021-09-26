492c6a7312ed8cd7ad4432a95f3f4ea7c52d0d6d
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-26 16:47:34 +0100

    Showing checkbox on color

--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -1,25 +1,34 @@
 import { FC } from 'react';
 import s from './Swatch.module.css';
 import { Check } from '@components/icons';
+import cn from 'classnames';
 
 interface Props {
   color?: string;
   label?: string;
+  active?: boolean;
   variant?: 'size' | 'color' | string;
   onClick: () => void;
 }
-const Swatch: FC<Props> = ({ color, label, variant, ...rest }) => {
+const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
   label = label?.toLowerCase();
   variant = variant?.toLowerCase();
+  const rootClassName = cn(s.root, {
+    [s.active]: active,
+    [s.color]: color,
+    [s.size]: variant === 'size',
+  });
   return (
     <button
-      className={s.root}
+      className={rootClassName}
       style={color ? { backgroundColor: color } : {}}
       {...rest}
     >
-      {/* <span>
-        <Check />
-      </span> */}
+      {variant === 'color' && active && (
+        <span>
+          <Check />
+        </span>
+      )}
       {variant === 'size' ? label : null}
     </button>
   );
