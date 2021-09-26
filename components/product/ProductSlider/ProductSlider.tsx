1a8a59d053d6d93cd82759ee9cefcd3d1d1f7962
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-26 15:55:10 +0100

    Changed slider margins

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,9 +1,10 @@
 import React, { FC, Children, isValidElement, useState } from 'react';
 import s from './ProductSlider.module.css';
+import cn from 'classnames';
 import { useKeenSlider } from 'keen-slider/react';
 
 const ProductSlider: FC = ({ children }) => {
-  const [sliderRef, _] = useKeenSlider({
+  const [sliderRef, slider] = useKeenSlider({
     initial: 0,
     loop: true,
     slideChanged(s) {
@@ -17,6 +18,14 @@ const ProductSlider: FC = ({ children }) => {
         ref={sliderRef as React.RefObject<HTMLDivElement>}
         className="h-full transition-opacity keen-slider"
       >
+        <button
+          onClick={slider?.prev}
+          className={cn(s.leftControl, s.control)}
+        ></button>
+        <button
+          onClick={slider?.next}
+          className={cn(s.rightControl, s.control)}
+        ></button>
         {Children.map(children, (child) => {
           if (isValidElement(child)) {
             return React.cloneElement(child, {
