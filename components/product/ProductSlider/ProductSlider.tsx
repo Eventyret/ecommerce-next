634775851680176c3ecf8191cec1cb1020a2bf3b
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-26 15:41:58 +0100

    Added cloneElement

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,10 +1,19 @@
-import { FC } from 'react';
+import React, { FC, Children, isValidElement } from 'react';
 import s from './ProductSlider.module.css';
 
 const ProductSlider: FC = ({ children }) => {
   return (
     <div className={s.root}>
-      <div className="h-full transition-opacity keen-slider">{children}</div>
+      <div className="h-full transition-opacity keen-slider">
+        {Children.map(children, (child) => {
+          if (isValidElement(child)) {
+            return React.cloneElement(child, {
+              className: 'keen-slider__slide',
+            });
+          }
+          return child;
+        })}
+      </div>
     </div>
   );
 };
