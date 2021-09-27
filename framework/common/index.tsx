67f546502bd290c62efde6de73f1d11180f47d8f
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-27 08:19:11 +0100

    Created apiProvider

---
+++ framework/common/index.tsx
@@ -0,0 +1,16 @@
+import { createContext, ReactNode, useContext } from 'react';
+import { getConfig } from '@framework/api/config';
+interface ApiProviderProps {
+  children: ReactNode | ReactNode[];
+}
+
+const config = getConfig();
+export const ApiContext = createContext({});
+
+export const ApiProvider = ({ children }: ApiProviderProps) => {
+  return <ApiContext.Provider value={config}>{children}</ApiContext.Provider>;
+};
+
+export const useApiProvider = () => {
+  return useContext(ApiContext);
+};
