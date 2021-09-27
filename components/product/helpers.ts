c00fe3562546f60e793b1bc30bad8c39f544c7d1
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-27 07:21:59 +0100

    Refactored helper function

--- components/product/helpers.ts
+++ components/product/helpers.ts
@@ -6,21 +6,13 @@ export type Choices = {
   [P in AvailableChoices]: string;
 };
 
-export function getVariant(product: Product, choices: Choices) {
-  const variant = product.variants.find((variant) => {
-    const isMatchingChoice = variant.options.every((variantOption) => {
+export const getVariant = (product: Product, choices: Choices) =>
+  product.variants.find((variant) =>
+    variant.options.every((variantOption) => {
       const optionName = variantOption.displayName.toLocaleLowerCase();
-      if (optionName in choices) {
-        if (choices[optionName] === variantOption.values[0].label) {
-          return true;
-        }
-      }
-
-      return false;
-    });
-
-    return isMatchingChoice;
-  });
-
-  return variant;
-}
+      return (
+        optionName in choices &&
+        choices[optionName] === variantOption.values[0].label
+      );
+    })
+  );
