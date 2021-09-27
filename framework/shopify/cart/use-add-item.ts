091abc885003a1d6e419355eea5c90feec866dda
Simen Daehlin <simen.dehlin@gmail.com>
2021-09-27 08:13:04 +0100

    Working on export and useaddItem functions

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,3 +1,15 @@
 import { useAddItem } from '@common/cart';
 
+export const handler = {
+  fetcher: () => {
+    console.log('Fetching Data');
+  },
+  useHook: () => {
+    return (input: any) => {
+      return {
+        output: JSON.stringify(input) + '_MODIFIED',
+      };
+    };
+  },
+};
 export default useAddItem;
